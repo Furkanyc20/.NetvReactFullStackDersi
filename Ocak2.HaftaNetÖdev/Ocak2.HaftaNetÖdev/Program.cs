4940530f33e57dcb35c139a18cedcef424d67465
Furkanyc20 <furkanyucel2012@hotmail.com>
2025-03-08 23:49:37 +0300

    Ocak2.HaftaNetÖdev

---
+++ Ocak2.HaftaNetÖdev/Ocak2.HaftaNetÖdev/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//2.Soru
+
+/*
+int x = 5;
+double y = 5.2;
+string name = "Mehmet";
+if (x < y)
+    Console.WriteLine(name + " verilen sayıilari dogru karşilastirdi");
+else
+    Console.WriteLine(name + " basarisiz oldu");
+*/
+
+//3.Soru
+
+/*
+int Sonuc;
+Console.WriteLine("Enter the numbers you want to add");
+Console.WriteLine("1.Number:"); int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2.Number:"); int number2 = Convert.ToInt32(Console.ReadLine());
+Sonuc = number1 + number2;
+Console.WriteLine("Sum of numbers:"+ Sonuc );
+*/
