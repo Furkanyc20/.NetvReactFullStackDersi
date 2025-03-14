7d574c59e2914cd852f02258f2c8c5112dd37e55
Furkanyc20 <furkanyucel2012@hotmail.com>
2025-03-14 22:54:10 +0300

    Subat1.HaftaÖdev

---
+++ Subat1.HatfaÖdev/Subat1.HatfaÖdev/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// 2.Soru
+
+/*
+int[] numbers = new int[5];
+
+for (int i = 0;  i < numbers.Length; i++)
+{
+    Console.Write($"Lütfen {i + 1}. sayıyı giriniz:");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(numbers[i]);
+}
+*/
+
+// 3.Soru
+
+/*
+int[] numbers = { 4, 45, 65, 99 };
+
+int max;
+max = numbers[0];
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max= numbers[i];
+    }
+}
+Console.WriteLine("Dizideki en büyük sayı: " + max);
+*/
