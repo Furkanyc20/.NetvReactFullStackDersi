8bca614e2855b48efed2eef3c62761d02851bdff
Furkanyc20 <furkanyucel2012@hotmail.com>
2025-03-14 22:33:45 +0300

    Ocak4.HaftaÖdev

---
+++ Ocak4.HaftaÖdev/Ocak4.HaftaÖdev/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// 2.Soru
+
+/*
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+*/
+
+// 3.Soru
+
+/*
+Console.WriteLine("Sayı giriniz:");
+int number = Convert.ToInt32(Console.ReadLine());
+int toplam = 0;
+for (int i = 0; i < number; i++)
+{
+    toplam = toplam + i;
+    
+}
+Console.WriteLine(toplam);
+*/
